d25952b7c6a4d487197ff6fc9c48cb6c50cb1636
Akshay Vs <akshay.vs2005@gmail.com>
2023-09-16 13:29:50 +0530

    Refactor: updated logs

--- .dev/cli/rafce.js
+++ .dev/cli/rafce.js
@@ -4,10 +4,11 @@ const { program } = require('commander');
 const fs = require('fs');
 const path = require('path');
 
-const loadTemplate = (file, component, callback) => {
-  const filePath = `./templates/${file}`;
-  const encoding = 'utf8';
+const readPath = path.join(__dirname, 'templates');
+const encoding = 'utf8';
 
+const loadTemplate = (file, component, callback) => {
+  const filePath = path.join(readPath, file);
   fs.readFile(filePath, encoding, (err, data) => {
     if (err) {
       console.error(`Error reading templates: ${err.message}`);
@@ -17,8 +18,6 @@ const loadTemplate = (file, component, callback) => {
 
     // Perform the replacements
     const modifiedData = data.replace(/{name}/g, `${component}`);
-    console.log(`Loading ${file}...`);
-
     callback(null, modifiedData); // Pass the modified data to the callback
   });
 };
@@ -40,40 +39,53 @@ program
   .arguments('<name>')
   .description('Create a component with the specified name')
   .action(async (name) => {
-    // Create a folder with the component name
-    const projectRoot = path.join(__dirname, '../..');
-    const componentDir = path.join(projectRoot, 'components', name);
-    fs.mkdirSync(componentDir);
+    try {
+      // Create a folder with the component name
+      const projectRoot = path.join(__dirname, '../..');
+      const componentDir = path.join(projectRoot, 'components', name);
+      fs.mkdirSync(componentDir);
 
-    console.log(`Component '${name}' created in '${componentDir}'.`);
+      const templates = {
+        'BaseTemplate.component.template': `${name}.tsx`,
+        'BaseTemplate.style.template': `${name}.style.scss`,
+        'BaseTemplate.mock.template': `${name}.mock.tsx`,
+        'BaseTemplate.stories.template': `${name}.stories.tsx`,
+        'BaseTemplate.index.template': `index.ts`,
+      };
 
-    const templates = {
-      'BaseTemplate.component.template': `${name}.tsx`,
-      'BaseTemplate.styles.template': `${name}.styles.scss`,
-      'BaseTemplate.mock.template': `${name}.mock.tsx`,
-      'BaseTemplate.stories.template': `${name}.stories.tsx`,
-      'BaseTemplate.index.template': `index.ts`,
-    };
+      const dirLen = await getDirLen(readPath);
+      for (let i = 0; i < dirLen; i++) {
+        const templateFile = Object.keys(templates)[i];
+        const newFileName = templates[templateFile];
 
-    const dirLen = await getDirLen('./templates');
-    for (let i = 0; i < dirLen; i++) {
-      const templateFile = Object.keys(templates)[i];
-      const newFileName = templates[templateFile];
-
-      loadTemplate(templateFile, name, (err, data) => {
-        if (err) {
-          console.error(`Error loading template: ${err.message}`);
-          return;
-        }
-
-        fs.writeFile(`${componentDir}/${newFileName}`, data, (err) => {
+        loadTemplate(templateFile, name, (err, data) => {
           if (err) {
-            console.error(`Error writing file: ${err.message}`);
+            console.error(`Error loading template: ${err.message}`);
             return;
           }
-          console.log(`File ${newFileName} written successfully.`);
+
+          fs.writeFile(`${componentDir}/${newFileName}`, data, (err) => {
+            if (err) {
+              console.error(`Error writing file: ${err.message}`);
+              return;
+            }
+          });
         });
-      });
+      }
+
+      console.log(
+        '\x1b[32m%s\x1b[0m',
+        `${name}' has been created successfully components directory.`
+      );
+    } catch (err) {
+      if (err.code === 'EEXIST') {
+        console.error(
+          '\x1b[31m%s\x1b[0m',
+          `File '${readPath}' already exists.`
+        );
+      } else {
+        console.error(`Error writing file: ${err.message}`);
+      }
     }
   });
 
